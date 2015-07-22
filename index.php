697c6bf92a02ae7bf85f25e0cb0910b40f3118c6
albert925 <albertarias925@gmail.com>
2015-07-22 11:39:19 -0500

    inicF1

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+	include 'config.php';
+	include 'fecha_format.php';
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -51,21 +55,33 @@
 					<li class="submen" data-num="1">
 						<a href="portafolio">Portafolio de servicios</a>
 						<ul class="children1">
-							<li><a href="">Crédito de Fomento</a></li>
-							<li><a href="">Crédito de Tesoreria</a></li>
-							<li><a href="">Crédito de Libranza</a></li>
-							<li><a href="">Administración de recursos</a></li>
-							<li><a href="">Factoring</a></li>
-							<li><a href="">Certificado de desarrollo territorial</a></li>
-							<li><a href="">Deposito a la vista</a></li>
-							<li><a href="">MicroCréditos</a></li>
-							<li><a href="">Otros Servicios</a></li>
+							<?php
+								$Tpf="SELECT * from mn_porf order by nam_po asc";
+								$sql_Tpf=mysql_query($Tpf,$conexion) or die (mysql_error());
+								while ($ffp=mysql_fetch_array($sql_Tpf)) {
+									$idpf=$ffp['id_mn_po'];
+									$nmpf=$ffp['nam_po'];
+							?>
+							<li><a href="portafolio/portaf.php?pf=<?php echo $idpf ?>"><?php echo "$nmpf"; ?></a></li>
+							<?php
+								}
+							?>
 						</ul>
 					</li>
 					<li class="submen" data-num="2">
 						<a href="perfil">Perfiles Directivos</a>
 						<ul class="children2">
-							<li><a href="">link 1</a></li>
+							<?php
+								$ttpfiles="SELECT * from tp_dic order by id_tpdc asc";
+								$sql_perfil=mysql_query($ttpfiles,$conexion) or die (mysql_error());
+								while ($fil=mysql_fetch_array($sql_perfil)) {
+									$idfil=$fil['id_tpdc'];
+									$nmfil=$fil['man_tpdc'];
+							?>
+							<li><a href="perfil/tpperfil.php?fl=<?php echo $idfil ?>"><?php echo "$nmfil"; ?></a></li>
+							<?php
+								}
+							?>
 						</ul>
 					</li>
 					<li><a href="boletin">Boletin Informativo</a></li>
@@ -113,15 +129,28 @@
 			</article>
 			<article id="todfrases">
 				<article class="owl-carousel-b owl-theme owl-loaded">
+					<?php
+						$frases="SELECT * from galeria order by id_gal desc";
+						$sql_frases=mysql_query($frases,$conexion) or die (mysql_error());
+						while ($fs=mysql_fetch_array($sql_frases)) {
+							$idSn=$fs['id_gal'];
+							$ttSn=$fs['tit_gal'];
+							$frSn=$fs['fra_gal'];
+							$txSn=$fs['txt_gal'];
+							$feSn=$fs['fe_gal'];
+					?>
 					<div class="item">
-						<hgropu>
-							<h2>Gestionar y optimizar la admiinistración de proyectos y recursos financieros</h2>
-							<h5>Claritas est etiam processus dynamicus, qui squitur mutationem consuetudium lectorum</h5>
-						</hgropu>
+						<hgroup>
+							<h2><?php echo "$ttSn"; ?></h2>
+							<h5><?php echo "$frSn"; ?></h5>
+						</hgroup>
 						<article>
-							<a href="#">Leer más</a>
+							<a href="frases.php?fs=<?php echo $idSn ?>">Leer más</a>
 						</article>
 					</div>
+					<?php
+						}
+					?>
 				</article>
 			</article>
 		</article>
@@ -134,6 +163,35 @@
 					<li><a href="nosotros">Quejas y Reclamos</a></li>
 					<li><a href="encuestas">Encuestas</a></li>
 					<li><a href="Formatos">Formatos</a></li>
+					<?php
+						$verMN="SELECT * from men_vert order by id_mv desc";
+						$sql_mnv=mysql_query($verMN,$conexion) or die (mysql_error());
+						while ($vv=mysql_fetch_array($sql_mnv)) {
+							$idmv=$vv['id_mv'];
+							$namv=$vv['nam_mv'];
+					?>
+					<li>
+						<a href="contenido.php?v=<?php echo $idmv ?>&bv=0"><?php echo "$namv"; ?></a>
+						<ul>
+							<?php
+								$subvv="SELECT * from sub_mv where mv_id=$idmv order by id_submv desc";
+								$sql_sbvv=mysql_query($subvv,$conexion) or die (mysql_error());
+								$numsbvv=mysql_num_rows($sql_sbvv);
+								if ($numsbvv>0) {
+									while ($sbvv=mysql_fetch_array($sql_sbvv)) {
+										$idsbmv=$sbvv['id_submv'];
+										$namsv=$sbvv['nam_submv'];
+							?>
+							<li><a href="contenido.php?v=<?php echo $idmv ?>&bv=<?php echo $idsbmv ?>"><?php echo "$namsv"; ?></a></li>
+							<?php
+									}
+								}
+							?>
+						</ul>
+					</li>
+					<?php
+						}
+					?>
 					<li><a href="">men</a>
 						<ul>
 							<li><a href="">men</a></li>
@@ -145,87 +203,115 @@
 				<figure id="galery">
 					<div class="slider-wrapper theme-default">
 						<div id="slider" class="nivoSlider">
-							<img src="imagenes/galery/fondo.jpg" alt="imagen" title="#caption1" />
+							<?php
+								$slid="SELECT * from slider order by id_sli desc";
+								$sql_slider=mysql_query($slid,$conexion) or die (mysql_error());
+								while ($ls=mysql_fetch_array($sql_slider)) {
+									$idSg=$ls['id_sli'];
+									$rtSg=$ls['rut_sli'];
+									$lkSg=$ls['lk_sli'];
+									if ($lkSg=="") {
+										$aruta="#";
+									}
+									else{
+										$aruta=$lkSg;
+									}
+							?>
+							<a href="<?php echo $aruta ?>">
+								<img src="<?php echo $rtSg ?>" alt="imagen_<?php echo $idSg ?>" title="#caption<?php echo $idSg ?>" />
+							</a>
+							<?php
+								}
+							?>
 						</div>
-						<div id="caption1" style="display:none;">
-							<h2>Texto</h2>
+						<?php
+							$txSlid="SELECT * from slider order by id_sli desc";
+							$sqltxs=mysql_query($txSlid,$conexion) or die (mysql_error());
+							while ($xxsl=mysql_fetch_array($sqltxs)) {
+								$BidSg=$xxsl['id_sli'];
+								$frSg=$xxsl['fra_sli'];
+						?>
+						<div id="caption<?php echo $BidSg ?>" style="display:none;">
+							<h2><?php echo "$frSg"; ?></h2>
 						</div>
+						<?php
+							}
+						?>
 					</div>
 				</figure>
 				<h1>Ifinorte</h1>
 				<article id="noticias">
+					<?php
+						error_reporting(E_ALL ^ E_NOTICE);
+						$tamno_pagina=15;
+						$pagina= $_GET['pagina'];
+						if (!$pagina) {
+							$inicio=0;
+							$pagina=1;
+						}
+						else{
+							$inicio= ($pagina - 1)*$tamno_pagina;
+						}
+						$ssql="SELECT * from noticias order by id_nt desc";
+						$rs=mysql_query($ssql,$conexion) or die (mysql_error());
+						$num_total_registros= mysql_num_rows($rs);
+						$total_paginas= ceil($num_total_registros / $tamno_pagina);
+						$gsql="SELECT * from noticias order by id_nt desc limit $inicio, $tamno_pagina";
+						$impsql=mysql_query($gsql,$conexion) or die (mysql_error());
+						while ($gh=mysql_fetch_array($impsql)) {
+							$idnt=$gh['id_nt'];
+							$nmnt=$gh['tit_nt'];
+							$rtnt=$gh['rut_nt'];
+							$rsnt=$gh['res_nt'];
+							$xtnt=$gh['txt_nt'];
+							$fent=$gh['fe_nt'];
+					?>
 					<figure>
-						<h3>Ifinorte esto un titulo</h3>
-						<a href="">
-							<img src="imagenes/galery/fondo.jpg" alt="iamgen" />
+						<h3><?php echo "$nmnt"; ?></h3>
+						<?php
+							if ($rtnt!="") {
+						?>
+						<a href="noticias.php?nt=<?php echo $idnt ?>">
+							<img src="<?php echo $rtnt ?>" alt="<?php echo $nmnt ?>" />
 						</a>
+						<?php
+							}
+						?>
 						<figcaption>
 							<p>
-								"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-								sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-								Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-								reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-								Excepteur sint occaecat cupidatat non proident, 
-								sunt in culpa qui officia deserunt mollit anim id est laborum."
+								<?php echo "$rsnt"; ?>
 							</p>
-							<a href="#">Leer más...</a>
+							<i><?php echo fechatraducearray($fent); ?></i>
+							<a href="noticias.php?nt=<?php echo $idnt ?>">Leer más...</a>
 						</figcaption>
 					</figure>
-					<figure>
-						<h3>Ifinorte esto un titulo</h3>
-						<a href="">
-							<img src="imagenes/galery/fondo.jpg" alt="iamgen" />
-						</a>
-						<figcaption>
-							<p>
-								"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-								sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-								Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-								reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-								Excepteur sint occaecat cupidatat non proident, 
-								sunt in culpa qui officia deserunt mollit anim id est laborum."
-							</p>
-							<a href="#">Leer más...</a>
-						</figcaption>
-					</figure>
-					<figure>
-						<h3>Ifinorte esto un titulo</h3>
-						<a href="">
-							<img src="imagenes/galery/fondo.jpg" alt="iamgen" />
-						</a>
-						<figcaption>
-							<p>
-								"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-								sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-								Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-								reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-								Excepteur sint occaecat cupidatat non proident, 
-								sunt in culpa qui officia deserunt mollit anim id est laborum."
-							</p>
-							<a href="#">Leer más...</a>
-						</figcaption>
-					</figure>
-					<figure>
-						<h3>Ifinorte esto un titulo</h3>
-						<a href="">
-							<img src="imagenes/galery/fondo.jpg" alt="iamgen" />
-						</a>
-						<figcaption>
-							<p>
-								"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-								sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-								Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-								reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-								Excepteur sint occaecat cupidatat non proident, 
-								sunt in culpa qui officia deserunt mollit anim id est laborum."
-							</p>
-							<a href="#">Leer más...</a>
-						</figcaption>
-					</figure>
+					<?php
+						}
+					?>
+				</article>
+				<article>
+					<br />
+					<b>Páginas: </b>
+					<?php
+						//muestro los distintos indices de las paginas
+						if ($total_paginas>1) {
+							for ($i=1; $i <=$total_paginas ; $i++) { 
+								if ($pagina==$i) {
+									//si muestro el indice del la pagina actual, no coloco enlace
+						?>
+							<b><?php echo $pagina." "; ?></b>
+						<?php
+								}
+								else{
+									//si el índice no corresponde con la página mostrada actualmente, coloco el enlace para ir a esa página 
+						?>
+									<a href="index.php?pagina=<?php echo $i ?>"><?php echo "$i"; ?></a>
+
+						<?php
+								}
+							}
+						}
+					?>
 				</article>
 			</section>
 		</article>
@@ -233,13 +319,31 @@
 	<section class="porfond">
 		<article id="automargen" class="prov">
 			<article class="owl-carousel owl-theme owl-loaded">
+				<?php
+					$proveCol="SELECT * from proveedor order by id_prov desc";
+					$sqlcolpvv=mysql_query($proveCol,$conexion) or die (mysql_error());
+					while ($clcv=mysql_fetch_array($sqlcolpvv)) {
+						$idcv=$clcv['id_prov'];
+						$nmcv=$clcv['nam_prov'];
+						$rtcv=$clcv['rut_prov'];
+						$lkcv=$clcv['lk_prov'];
+						if ($lkcv=="") {
+							$vclk="#";
+						}
+						else{
+							$vclk=$lkcv;
+						}
+				?>
 				<div class="item">
 					<figure>
-						<a href="#" target="_blank">
-							<img src="imagenes/provedores/aa.png" alt="aa" />
+						<a href="<?php echo $vclk ?>" target="_blank">
+							<img src="<?php echo $rtcv ?>" alt="<?php echo $nmcv ?>" />
 						</a>
 					</figure>
 				</div>
+				<?php
+					}
+				?>
 			</article>
 		</article>
 	</section>
